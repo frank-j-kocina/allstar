d661dd0c91733dc0b50351deca1fd7f32b6a52cd
Frank J Kocina <frank.j.kocina@gmail.com>
2012-07-29 12:42:02 -0500

    add initial files, starting fresh

---
+++ js/allstar.js
@@ -0,0 +1,6 @@
+$('#services-tab').on('click', function() {
+  $('#home-content').addClass('hide');
+  $('#services-content').removeClass('hide')";
+  $('#services-tab').removeClass('inactive-tab');
+  $('#services-tab').addClass('active-tab');
+});
