8ab654f3fbb3e43ed9eb9fd52f6011f70f0dabc9
Frank J Kocina <frank.j.kocina@gmail.com>
2012-07-29 20:43:48 -0500

    custom tab implementation working

--- js/allstar.js
+++ js/allstar.js
@@ -1,6 +1,25 @@
-$('#services-tab').on('click', function() {
-  $('#home-content').addClass('hide');
-  $('#services-content').removeClass('hide')";
-  $('#services-tab').removeClass('inactive-tab');
-  $('#services-tab').addClass('active-tab');
+$(document).ready(function() {
+  activateTabAndShowContent('home');
+  $('#home-tab').on('click', {id: 'home'}, activateTabAndShowContent);
+  $('#services-tab').on('click', {id: 'services'}, activateTabAndShowContent);
+  $('#photos-tab').on('click', {id: 'photos'}, activateTabAndShowContent);
+  $('#contact-tab').on('click', {id: 'contact'}, activateTabAndShowContent);
 });
+
+function activateTabAndShowContent(event) {
+  var id = event;
+
+  if(event.data)
+    if(event.data.id)
+      id = event.data.id
+
+  var tabId = '#' + id + '-tab';
+  var contentId = '#' + id + '-content';
+
+  $('.tab').addClass('inactive-tab')
+  $(tabId).removeClass('inactive-tab');
+  $(tabId).addClass('active-tab');
+
+  $('.content').hide();
+  $(contentId).show();
+}
