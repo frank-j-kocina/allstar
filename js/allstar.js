bde16f1e459c6f275343e45ad59a782655e84e91
Frank J Kocina <frank.j.kocina@gmail.com>
2012-08-01 21:57:30 -0500

    move google analytics from html to js, add fadeshow with dummy content, change prettyphoto name

--- js/allstar.js
+++ js/allstar.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+  runGoogleAnalytics();
   activateTabAndShowContent('home');
+  runFadeshow();
   $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false, overlay_gallery: false, slideshow: 3000});
 
   $('#home-tab').on('click', {id: 'home'}, activateTabAndShowContent);
@@ -7,6 +9,17 @@ $(document).ready(function() {
   $('#photos-tab').on('click', {id: 'photos'}, activateTabAndShowContent);
   $('#contact-tab').on('click', {id: 'contact'}, activateTabAndShowContent);
 });
+
+function runGoogleAnalytics() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22476759-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
 
 function activateTabAndShowContent(event) {
   var id = event;
@@ -25,4 +38,14 @@ function activateTabAndShowContent(event) {
 
   $('.content').hide();
   $(contentId).show();
+}
+
+function runFadeshow() {
+  $('#fadeshow').crossSlide({
+    sleep: 2,
+    fade: 1
+  }, [
+    { src: 'images/gallery/fullsize/greyAllstar.jpg' }, // TODO - set up images
+    { src: 'images/gallery/fullsize/greyGlass.jpg'   }
+  ]);
 }
